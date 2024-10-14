291c3b72a8824944e68630ade80dde36ee26f33f
DripTrace <rpalm@driptrace.io>
2024-10-14 02:00:05 -0700

    domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+    // images: {
+    //     domains: ["raw.githubusercontent.com", "poke-assessment.driptrace.io"],
+    // },
     images: {
-        domains: ["raw.githubusercontent.com", "poke-assessment.driptrace.io"],
-    },
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "raw.githubusercontent.com",
+                pathname: "**",
+            },
+            {
+                protocol: "https",
+                hostname: "poke-assessment.driptrace.io",
+                pathname: "**",
+            },
+        ]
+    }
 }
     ;
 
